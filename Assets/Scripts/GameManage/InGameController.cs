44d49c673415e48f80888ccb4fc143075763d3ac
cyo2 <108583303+cyo2@users.noreply.github.com>
2023-08-07 09:32:16 +0700

    fix2 7/8

--- Assets/Scripts/GameManage/InGameController.cs
+++ Assets/Scripts/GameManage/InGameController.cs
@@ -190,7 +190,11 @@ public class InGameController : MonoBehaviour
         AddBallActive();
         for (int i = 0; i < ballActiveList.Count; i++) 
         { 
+<<<<<<< HEAD
             //Debug.Log("MULTIPLY BALL" + i );
+=======
+            Debug.Log("MULTIPLY BALL" + i );
+>>>>>>> parent of 5949819 (change 4/8)
             ballActiveList[i].BallMultiply();
         }
     }
